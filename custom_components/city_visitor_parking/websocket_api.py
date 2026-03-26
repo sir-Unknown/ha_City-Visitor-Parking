eae9fae5c6cdbb9fd1c82d670c15ca37f6d1234c
sir-Unknown <sir-Unknown@users.noreply.github.com>
2026-03-26 23:43:19 +0000

    fix: rely on coordinator typing in websocket status

--- custom_components/city_visitor_parking/websocket_api.py
+++ custom_components/city_visitor_parking/websocket_api.py
@@ -14,7 +14,6 @@ from homeassistant.util import dt as dt_util
 from pycityvisitorparking.exceptions import PyCityVisitorParkingError
 
 from .const import DOMAIN
-from .models import CoordinatorData
 from .payloads import build_status_payload, normalize_favorites
 
 if TYPE_CHECKING:
